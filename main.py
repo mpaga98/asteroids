f91b2177e00de857d1cc6556ba8e93a4d41d60b1
mpaga98 <mpagan1398@gmail.com>
2025-01-31 15:17:18 -0500

    Added constants, edited main.py to include game loop

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+    
 
 if __name__ == "__main__":
     main()
