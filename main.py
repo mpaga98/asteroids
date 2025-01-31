bcc670128af1bf2c9ac1f759584fac9d04ee0a25
mpaga98 <mpagan1398@gmail.com>
2025-01-31 15:54:38 -0500

    Added delta time handler in main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
@@ -14,6 +16,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        time.tick(60)
+        dt = time.get_time()
+        dt /= 1000
     
 
 if __name__ == "__main__":
