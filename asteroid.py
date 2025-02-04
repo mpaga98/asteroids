d53a85a1f00a4eda28e21687c2161895e9c4a4d9
mpaga98 <mpagan1398@gmail.com>
2025-02-04 00:09:42 -0500

    Created split method in asteroid class, allowing for asteroids to split into smaller ones when a collision with shot objects are found

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 #base class for asteroids object
 class Asteroid(CircleShape):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle = random.uniform(20, 50)
+            
+        vec1 = self.velocity.rotate(angle)
+        vec2 = self.velocity.rotate(-angle)
+            
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+            
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            
+        asteroid1.velocity = vec1 * 1.2
+        asteroid2.velocity = vec2 * 1.2
